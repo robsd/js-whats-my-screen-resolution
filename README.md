08c1d150f21355ba04c477628fbf447e49ae15db
Robert Dixon <79385125+robsd@users.noreply.github.com>
2021-10-21 01:01:56 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# js-whats-my-screen-resolution 🖥
+
+A super simple JavaScript application that tells you the screen resolution of your device.
+
+## Demo
+
+https://robsd.github.io/js-whats-my-screen-resolution
---
+++ js/script.js
@@ -0,0 +1,3 @@
+document.getElementById('width').innerHTML = screen.width;
+document.getElementById('height').innerHTML = screen.height;
+document.getElementById('resolution').innerHTML = screen.width + ' x ' + screen.height;
